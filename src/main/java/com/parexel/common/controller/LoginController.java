ec43fab440deb28c9153b58b40da088d0aa07685
Rajesh Kumar Yuvaraj <rajeshkumar.yuvaraj@yahoo.com>
2017-09-22 14:18:51 +0530

    Initial commit
    Initial commit

---
+++ src/main/java/com/parexel/common/controller/LoginController.java
@@ -0,0 +1,31 @@
+package com.parexel.common.controller;
+
+import java.security.Principal;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LoginController {
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	public String welcome(ModelMap model, Principal principal) {
+		String name = principal.getName();
+		model.addAttribute("username", name);
+		model.addAttribute("message", "Single Singon welcomes you...!!");
+		
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		if (!(authentication instanceof AnonymousAuthenticationToken)) {
+		    String currentUserName = authentication.getName();
+		   System.err.println(" currentUserName :::"+currentUserName);
+		   
+		   
+		   System.err.println(authentication.getCredentials());
+		}
+		return "welcome";
+	}
+}
---
+++ target/SSO/WEB-INF/pages/welcome.jsp
@@ -0,0 +1,7 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<html>
+<body>
+	<h3>Message : ${message}</h3>
+	<h3>Username : ${username}</h3>
+</body>
+</html>
